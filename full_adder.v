579a3f1b849a5e76c063339b6bfa65f324a631b3
Caruntu <vlad.caruntu@student.upt.ro>
2026-04-20 14:59:39 +0300

    COmentarii RCA

--- full_adder.v
+++ full_adder.v
@@ -6,9 +6,9 @@ module full_adder (
     output wire cout
 );
     wire xor1, and1, and2;
-    xor g1 (xor1, a, b);
-    xor g2 (sum,  xor1, cin);
+    xor g1 (xor1, a, b);// defineste poarta xor intre a si b
+    xor g2 (sum,  xor1, cin);// suma= (a xor b) xor cin
     and g3 (and1, a, b);
     and g4 (and2, xor1, cin);
-    or  g5 (cout, and1, and2);
+    or  g5 (cout, and1, and2);//carryoutul= (a AND b) OR ((a XOR b) AND cin)
 endmodule
